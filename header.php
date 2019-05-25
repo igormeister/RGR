17752d56b61ab535dad487bf4ecaaae332cc536e
romusk <ilienkors@gmail.com>
2019-05-25 17:24:35 +0300

    Index page frontend

--- header.php
+++ header.php
@@ -1,196 +1,22 @@
-<?php
-session_start();
-$link  = mysqli_connect("127.0.0.1", "root", "", "web");
-if ($_SERVER['PHP_SELF'] == '/editQuestion.php') {
-  $id = $_GET['id'];
-  $sql = "SELECT * FROM `question` WHERE id='$id'";
-  $chek = mysqli_fetch_array(mysqli_query($link, $sql));
-  if ($chek['user_id'] != $_SESSION['userId']) {
-    header('Location: index.php');
-  }
-}
-if (isset($_SESSION['userId'])) {
-  $currentId = $_SESSION['userId'];
-  $query = "SELECT `id`, `name`, `surname`, `login`, `email`, `password` FROM `person` WHERE `id`=$currentId";
-  $person = mysqli_fetch_array(mysqli_query($link, $query));
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="js/tinymce/jquery.tinymce.min.js"></script>
-  <script src="js/tinymce/tinymce.min.js"></script>
-  <script>
-    tinymce.init({
-      selector: 'textarea'
-    })
-  </script>
-  <title>YourCode.com</title>
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">YourCode</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <?php if (!isset($_SESSION['userId'])) : ?>
-          <li class="nav-item active" style="margin-right:10px">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#signInModal">
-              Log in
-            </button>
-          </li>
-          <li class="nav-item">
-            <button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#signUpModal">
-              Sign up
-            </button>
-          </li>
-        <?php endif; ?>
-        <?php if (isset($_SESSION['userId'])) : ?>
-          <li class="nav-item">
-            <a href="system/logOut.php" type="button" class="btn btn-outline-light">
-              Log out
-            </a>
-          </li>
-        <?php endif; ?>
-      </ul>
-      <form class="form-inline mt-2 mt-md-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
-  <!-- Modal -->
-  <div class="modal fade" id="signInModal" tabindex="-1" role="dialog" aria-labelledby="signInModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="signInModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#0094f9">
+        <a class="navbar-brand" href="#">YourCode</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="btn btn-light" href="#">Log in</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="btn btn-light" href="#">Log out</a>
+                </li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-        <div class="modal-body">
-          <form action="system/autorization.php" method="POST">
-            <div class="form-group">
-              <label for="signInInputEmail">Email address</label>
-              <input type="email" class="form-control" id="signInInputEmail" aria-describedby="emailHelp" placeholder="Enter email" name='email'>
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                else.</small>
-            </div>
-            <div class="form-group">
-              <label for="signInInputPassword">Password</label>
-              <input type="password" class="form-control" id="signInInputPassword" placeholder="Password" name='password'>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="signUpModal" tabindex="-1" role="dialog" aria-labelledby="signUpModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="signUpModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form action="system/registration.php" method='POST'>
-            <div class="form-group">
-              <label for="signUpInputEmail">Email address</label>
-              <input type="email" class="form-control" id="signUpInputEmail" aria-describedby="emailHelp" placeholder="Enter email" name='email'>
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                else.</small>
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="login" name='login'>
-            </div>
-            <div class="form-row">
-              <div class="col">
-                <input type="text" class="form-control" placeholder="First name" name='name'>
-              </div>
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Last name" name='surname'>
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="signUpInputPassword">Password</label>
-              <input type="password" class="form-control" id="signUpInputPassword" placeholder="Password" name='password'>
-              <label for="signUpInputPassword">Repeat password</label>
-              <input type="password" class="form-control" id="signUpInputPassword" placeholder="Password" name='repeatPassword'>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container-fluid" style="margin-top: 50px;background-color:#dcdbdb;">
-    <div class="row">
-      <div class="col-md-2">
-        <div class="row" style="min-height:100vh; padding:7px; margin-top: 10px;">
-          <div class="col-md-12 shadow p-3 mb-5 bg-white rounded">
-            <?php if (isset($_SESSION['userId'])) : ?>
-              <div class="jumbotron">
-                <h3>Hello,
-                  <? echo $person['login'] ?>!</h3>
-                <p class="lead">Name
-                  <? echo $person['name'] ?>
-                </p>
-                <p class="lead">Surname
-                  <? echo $person['surname'] ?>
-                </p>
-                <p class="lead">email
-                  <? echo $person['email'] ?>
-                </p>
-                <p class="lead">Comments
-                  <? echo $_SESSION['userId'] ?>
-                </p>
-                <hr class="my-4">
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Show my question</button>
-              </div>
-            <?php endif; ?>
-            <?php if (isset($_SESSION['userId'])) : ?>
-              <div class="row">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModalCenter">Change
-                  profile data</button>
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        ...
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row" style="margin-top:10px">
-                <a href="question.php" type="button" class="btn btn-primary btn-lg btn-block">Create new question</a>
-              </div>
-            <?php endif; ?>
-          </div>
-        </div>
-      </div>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,37 +1,121 @@
-<?php include_once('header.php'); ?>
-<?php
-$query = 'SELECT * FROM `question`';
-$arrayQuestion = mysqli_query($link, $query);
-?>
-<div class="col-md-8">
-  <div class="row" style="min-height:100vh; padding:7px; margin-top: 10px;">
-    <div class="col-md-12 shadow p-3 mb-5 bg-white rounded">
-      <div class="row" style="margin-bottom:10px; margin-top: 10px">
-        <div class="col-md-12">
-          <ul class="nav nav-pills justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Date</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Comment</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <?php foreach ($arrayQuestion as $question) : ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>YourCode</title>
+    <?php include_once('css-links.php') ?>
+</head>
+
+<body>
+    <?php include_once('header.php'); ?>
+    <div class="container-fluid">
         <div class="row">
-          <div class="col-md-12">
-            <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="min-width:100%">
-              <div class="card-body">
-                <h5 class="card-title">Question title <?= $question['title'] ?> <span style="float: right"><?= $question['date'] ?></span></h5>
-                <p class="card-text"></p>
-                <a href="questionPage.php?id=<?= $question['id'] ?>" class="btn btn-primary">Go to question</a>
-              </div>
+            <div class="col-md-2 example-height">
+                <div class="inner-block">
+                    <div class="left-inner-inner-block">
+                        <img src="img/avatars/1.png" alt="..." class="img-thumbnail">
+                        <h4>Hello, User!</h4>
+                    </div>
+                    <button type="button" class="btn btn-warning" style="margin-top:10px; width: 100%">Create question</button>
+                    <button type="button" class="btn btn-warning" style="margin-top:10px; width: 100%">Your questions</button>
+                </div>
             </div>
-          </div>
+            <div class="col-md-7 example-height">
+                <div class="inner-block">
+                <h4 style="color:#0094f9">All questions:</h4>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 example-height">
+                <div class="inner-block">
+                    <h4 style="color:#0094f9">Unresolved questions:</h4>
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      <? endforeach ?>
     </div>
-  </div>
-</div>
-<?php include_once('footer.php'); ?>
+
+    <div class="container-fluid footer-container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <footer class="page-footer font-small blue copFooter">
+                    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                        <a href=""> Tatyana Kovaluk</a>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+
+</body>
+
+</html>
